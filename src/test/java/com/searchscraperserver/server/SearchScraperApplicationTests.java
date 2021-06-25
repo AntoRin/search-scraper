56479567d5e5bd9e719061e038da96d168f54558
AntoRin_ux <antorinish@gmail.com>
2021-06-25 22:12:49 +0530

    change file name

--- src/test/java/com/searchscraperserver/server/SearchScraperApplicationTests.java
+++ src/test/java/com/searchscraperserver/server/SearchScraperApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StackCrawlerApplicationTests {
+class SearchScraperApplicationTests {
 
 	@Test
 	void contextLoads() {
